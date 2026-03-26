dfa77245b909baa9cd1a2186757c34373b14b2c4
ie-aou <bowl.xs6@gmail.com>
2026-03-26 23:59:39 +0900

    Java 25へのバージョン修正とテスト用コントローラー作成しました

---
+++ src/main/java/com/example/todo_app/Demo.java
@@ -0,0 +1,14 @@
+package com.example.todo_app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Demo {
+    
+    @GetMapping("test")
+    public String getMethodName() {
+        return "test";
+    }
+
+}
