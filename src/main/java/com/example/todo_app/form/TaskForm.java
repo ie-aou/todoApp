0162a0175ffb9d0ca587d2c2cf4363861c8d1523
ie-aou <bowl.xs6@gmail.com>
2026-04-12 02:06:56 +0900

    ディレクトリとパッケージを変更しました

--- src/main/java/com/example/todo_app/form/TaskForm.java
+++ src/main/java/com/example/todo_app/form/TaskForm.java
@@ -1,12 +1,12 @@
-package com.example.todo_app;
+package com.example.todo_app.form;
 import java.util.Date;
 import lombok.Data;
 
 @Data
 public class TaskForm {
-    private String name;
-    private Date startDate;
-    private Date dueDate;
+    private String title;
+    private String startDate;
+    private String dueDate;
     private String priority;
     private String detail;
 }    
