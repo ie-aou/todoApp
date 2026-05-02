c1bde6e9e648ac6eb86c151c5e443009858974a7
ie-aou <bowl.xs6@gmail.com>
2026-05-02 23:58:35 +0900

    sqlを使うための設定をしました。Entityを実装するクラスを作成中です。

---
+++ src/main/java/com/example/todo_app/entity/Task.java
@@ -0,0 +1,10 @@
+package com.example.todo_app.entity;
+
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Task")
+public class Task {
+    
+}
